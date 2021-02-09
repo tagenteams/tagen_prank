a532041fef16d8cd59fede36808e8e4aa84a64f3
tagenteams <tagenteams@gmail.com>
2021-02-09 23:12:11 +0900

    初回コミット

---
+++ setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="tagen_prank",
+    version="0.1.0",
+    author="tagenteams",
+    author_email="UNKNOWN",
+    description="You can receive the message 'なにィ!!'",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tagenteams/tagen_prank",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    entry_points = {
+        'console_scripts': ['tagen_prank = tagen_prank.tagen_prank:main']
+    },
+    python_requires='>=3.7',
+)
---
+++ tagen_prank/tagen_prank.py
@@ -0,0 +1,5 @@
+def main():
+    print("なにィ!!")
+
+if __name__ == '__main__':
+    main()
