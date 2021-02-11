03b2fa87250a70b6eb1ddf5e9990005bb6f16b43
tagenteams <tagenteams@gmail.com>
2021-02-11 16:34:04 +0900

    表示文言追加

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tagen_prank",
-    version="0.2.0",
+    version="0.3.0",
     author="tagenteams",
     author_email="UNKNOWN",
     description="You can receive the message 'なにィ!!!'",
--- tagen_prank/tagen_prank.py
+++ tagen_prank/tagen_prank.py
@@ -1,15 +1,27 @@
+import random
 
 class TagenPrank(object):
     """Prank library
     """
 
-    def __init__(self, input_word: str):
+    def __init__(self, input_word: str = 'テストなんさなあ'):
         self._input_word = input_word
 
     @staticmethod
-    def say_nanii():
-        print('なにィ!!!')
+    def captain():
+        word_li = [
+            'なにィ!!!',
+            'くっ!ガッツがたりない! ',
+            'サッカーは格闘技',
+            '若林源三の噛ませ犬じゃないんだ！！！',
+            '単なるデブは全日本にはいらない',
+            'お前こそが日本のSGGKだ！',
+            '森崎くんだからとれなーい！',
+            'もりさきくん　するどいパンチング！　だが、とどかない！',
+            '森崎もだ！こんなシュート若林くんならかるく胸でキャッチしてるぞ！',
+        ]
+        print(random.choice(word_li))
 
 def main():
-    prank = TagenPrank('テストなんさなあ')
-    prank.say_nanii()
+    prank = TagenPrank()
+    prank.captain()
